482821677e0796a523558c06fad0dd705b76854d
Rene Lee Aquitania <aquitaniarlt@aub.com.ph>
2025-09-29 17:22:55 +0800

    [master]
    - Tailwind integration
    - Added initial banner
    - Removed default React files

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div>
+            <div className="bg-[url(/src/assets/banner.png)] h-min w-screen bg-cover bg-no-repeat bg-center border-2 border-blue-400">
+                <h1>Hello world</h1>
+            </div>
+            <h1>Test</h1>
+        </div>
+    );
+}
